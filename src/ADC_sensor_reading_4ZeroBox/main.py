2abaa04d035df83fcb8f3b56fff19d7dc9336a36
Ugo Scarpellini <u.scarpellini@zerynth.com>
2021-03-22 09:26:49 +0000

    Aggiornare 'src/ADC_sensor_reading_4ZeroBox/main.py'
    Sensor Reading - Tested example

--- src/ADC_sensor_reading_4ZeroBox/main.py
+++ src/ADC_sensor_reading_4ZeroBox/main.py
@@ -3,7 +3,6 @@
 ################################################################################
 
 import mcu
-import json
 import threading as th
 import watchdog
 from bsp import board
@@ -31,13 +30,17 @@ def read_event_handler():
             core_sample_lock.acquire()
             ready = False
             print("======== reading")
-            # Read from 4-20mA channel1, resistive channel1, power channel1
+            # Read from 4-20mA channel1, resistive channel1/channel2/channel3, power channel1
             analog_val = board.read_420(1)
-            temperature = board.read_resistive(1)
-            power = board.read_power(1, 0)
-            print(" - analog:", analog_val)
-            print(" - temp:", temperature)
-            print(" - power:", power)
+            temp_1 = board.read_resistive(1)
+            temp_2 = board.read_resistive(2)
+            temp_3 = board.read_resistive(3)
+            power = board.read_power(1)
+            print(" - analog    :", analog_val)
+            print(" - temp 1    :", temp_1)
+            print(" - temp 2    :", temp_2)
+            print(" - temp 3    :", temp_3)
+            print(" - power     :", power)            
             print("======== done")
         except Exception as e:
             print("Generic Error:", e)
@@ -46,14 +49,18 @@ def read_event_handler():
     
 try:
     print("adc config...")
-    # Config FourZeroBox ADC channels        
-    board.config_adc(0, 1, 3, 0)
-    board.config_adc(1, 1, 2, 0)
-    board.config_adc(2, 1, 2, 7)
+    # Config FourZeroBox ADC channels: config_adc(label, ch, pga, sps)
+    board.config_adc(board.ADC_010_420, 1, 2, 7)
+    board.config_adc(board.ADC_RES, 1, 2, 7)
+    board.config_adc(board.ADC_RES, 2, 2, 7)
+    board.config_adc(board.ADC_RES, 3, 2, 7)
+    board.config_adc(board.ADC_CUR, 1, 2, 7)
     # Config FourZeroBox ADC conversion parameters
-    board.set_conversion_010_420(1, 0, 100, 0, 0, 100)
-    board.set_conversion_resistive(1, -50, ref_table, 5)
-    board.set_conversion_current(1, 100, 5, 2000, 220, 0)
+    board.set_conversion_010_420(1, 0, 100, 0, 0, 100)      # (ch, y_min, y_max, offset=0, under_x=None, over_x=None)
+    board.set_conversion_resistive(1, -50, ref_table, 5)    # (ch, v_min, ref_table, delta, offset=0)
+    board.set_conversion_resistive(2, -50, ref_table, 5)
+    board.set_conversion_resistive(3, -50, ref_table, 5)
+    board.set_conversion_current(1, 150, 1, 3000, 230, 0)   # (ch, n_samples=400, ncoil=1, ratio=2000, voltage=220, offset=0)
     print("adc config done")
 except Exception as e:
     print(e)
@@ -65,7 +72,7 @@ try:
     print('start main')
     # Main Loop
     while True:
-        sleep(5000)
+        sleep(1000)
         # Sync between main thread and pub_event_handler thread 
         if ready:
             core_sample_lock.release()
