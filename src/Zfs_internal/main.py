78d722c121f33ca95ebf7c3294e60689e878e4f9
s.falchi <s.falchi@zerynth.com>
2021-04-07 10:28:31 +0200

    fix for ZFileSystem internal example

--- src/Zfs_internal/main.py
+++ src/Zfs_internal/main.py
@@ -5,29 +5,19 @@
 import fs
 
 while True:
-    fs.mount("/mnt")
 
-    #read and write an existing file
-    f = fs.FileIO("mnt/test.txt", "r")
+    #read an existing file
+    f = fs.open("/zerynth/test.txt", "r")
     print(f.read())
     f.close()
 
-    f = fs.FileIO("/mnt/test.txt","w")
-    f.write("first row: test 01")
-    f.write("second row: test 02")
-    print(f.readline())
-    print(f.readline())
-    f.close()
-    
     # create a new file and read it back
-    f = fs.FileIO("/mnt/test02.txt","w")
-    f.write("Hello Zerynth!")
+    f = fs.open("/zerynth/test02.txt","w")
+    f.write("first row: test 01\n")
+    f.write("second row: test 02\n")
     f.close()
-
-    f = fs.FileIO("/mnt/test02.txt","r")
+    f = fs.open("/zerynth/test02.txt", "r")
     print(f.read())
     f.close()
-
-    fs.unmount("/mnt")
 
     sleep(1000)
