7fa6c3b65305d59cbfaf2cd31c42704e0d3d51af
Elia Guglielmin <e.guglielmin@zerynth.com>
2021-04-19 12:11:56 +0200

    exp-ser: updated example for rs232 with two boards
    now the example works with two boards exchanging
    a ping pong.
    
    Signed-off-by: Elia Guglielmin <e.guglielmin@zerynth.com>

--- src/Exp_ser_rs232/main.py
+++ src/Exp_ser_rs232/main.py
@@ -10,79 +10,26 @@ from bsp import board
 
 from expansions import ser
 import serial
-import mcu
-import watchdog
-import threading as th
+import msg_exchange
+
 
 # Initialize board
 board.init()
 # Pass the rotative switch selector position
 # Interrupt pin INTR
-ser_sw_sel1 = (0,)
+ser_sw_sel = (0,)
 # Add the EXP-SER to the board
 # All pin of the EXP-SER will be initialized correctly
 exp_ser1 = board.next_expansion(ser, ser_sw_sel)
-# Initialize serial1 and serial2
+# Initialize serial1
 ser1 = exp_ser1.get_serial(ser=SERIAL1, mode=serial.MODE_UART, flow_ctrl=serial.HW_FLOWCTRL_CTS_RTS)
 
-ser_sw_sel2 = (0,)
-exp_ser2 = board.next_expansion(ser, ser_sw_sel2)
-ser2 = exp_ser.get_serial(ser=SERIAL2, mode=serial.MODE_UART, flow_ctrl=serial.HW_FLOWCTRL_CTS_RTS)
+while True:
+    # From msg_exchange call the ping_starter function on one board
+    # and ping_receiver on the other to start a PING-PONG between the two.
 
-# Lock for sync
-core_sample_lock = th.Lock()
-ready = True
+    # Uncomment to create the PING sender
+    msg_exchange.ping_starter(ser1)
 
-# Set Watchdog timeout
-watchdog.setup(60000)
-
-# Thread function to write 
-def write_handler():
-    global ready
-
-    while True:
-        try:
-            # Sync
-            ready = True
-            core_sample_lock.acquire()
-            ready = False
-            # Write a message
-            temp = ser1.write("Hello Zerynth!")
-        except Exception as e:
-            print("Generic Error:", e)
-            board.error_cloud()
-            mcu.reset()
-
-# Thread function to read 
-def read_handler():
-    global ready
-
-    while True:
-        try:
-            # Sync
-            ready = True
-            core_sample_lock.acquire()
-            ready = False
-            # Read a message
-            temp = ser2.read()
-            print("Message: ", temp)
-        except Exception as e:
-            print("Generic Error:", e)
-            board.error_cloud()
-            mcu.reset()
-
-try:
-    print("Start threads")
-    thread(write_handler())
-    thread(read_handler())
-    print("Start main")
-    while True:
-        sleep(3000)
-        # Sync between main thread and pub_event_handler thread 
-        if ready:
-            core_sample_lock.release()
-            # Reset Watchdog timer
-            watchdog.kick()
-except Exception as e:
-    print (e)
-    mcu.reset()
+    #Uncomment to create the PING receiver
+    # msg_exchange.ping_receiver(ser1)
