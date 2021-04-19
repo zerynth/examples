ff34888191aa1d01a8cbef170af688e5bf797994
Elia Guglielmin <e.guglielmin@zerynth.com>
2021-04-19 15:46:17 +0200

    exp-io: adc example updated
    shrinked the example and fixed json.
    
    Signed-off-by: Elia Guglielmin <e.guglielmin@zerynth.com>

--- src/Exp_Io_ADC/main.py
+++ src/Exp_Io_ADC/main.py
@@ -10,11 +10,6 @@ from bsp import board
 
 from expansions import io
 from zsensors import sensor
-import watchdog
-import threading as th
-import gpio
-import adc
-import mcu
 
 # Initialize board
 board.init()
@@ -27,53 +22,13 @@ io_sw_sel = (1, 0,)
 # All pin of the EXP-IO will be initialized correctly
 exp_io = board.next_expansion(io, io_sw_sel)
 
-# Lock for sync
-core_sample_lock = th.Lock()
-ready = True
+# Create a dictionary with all sensors
+# Sensors can be accesses using the key passed on the
+# json configuration file ("temperature" in this case)
+sens_dict = sensor.get_sensors_dict()
+print("Created sensor class")
 
-# Set Watchdog timeout
-watchdog.setup(60000)
-
-# Thread function for read sensor data and send them to the cloud
-def read_event_handler():
-    global ready
-
-    while True:
-        try:
-            # Sync
-            ready = True
-            core_sample_lock.acquire()
-            ready = False
-            print("======== reading")
-            # Read from 4-20mA channel1, resistive channel1/channel2/channel3, power channel1
-            temp = exp_io.read_resistive(1)
-            print(" - temp    :", temp)        
-            print("======== done")
-        except Exception as e:
-            print("Generic Error:", e)
-            board.error_cloud()
-            mcu.reset()
-    
-try:
-    print("adc config...")
-    #Create a dictionary with all sensors
-    d = sensor.get_sensors_dict()
-    print("adc config done")
-except Exception as e:
-    print(e)
-    mcu.reset()
-
-try:
-    print("Start read_event_handler thread")
-    thread(read_event_handler())
-    print("start main")
-    while True:
-        sleep(1000)
-        # Sync between main thread and pub_event_handler thread 
-        if ready:
-            core_sample_lock.release()
-            # Reset Watchdog timer
-            watchdog.kick()
-except Exception as e:
-    print (e)
-    mcu.reset()
+while True:
+    # Read and print the current temperature read by the sensor
+    print("temperature:", sens_dict["temperature"].read())
+    sleep(1000)
