8ee4bd9d60732d22b4b56c5822e2f93edcdf61a1
Marco Gigante <m.gigante@zerynth.com>
2021-10-06 17:38:52 +0200

    Added ZDM w/ cellular and gps examples

---
+++ src/ZDM_Cellular_GPS/main.py
@@ -0,0 +1,103 @@
+################################################################################
+# ZDM Cellular with GPS
+################################################################################
+
+# First, import the board module from the bsp (board support package).
+# The bsp loads board specific names, variables and settings so that
+# this project can be run on different Zerynth hardware
+# without changing a line of code.
+from bsp import board
+
+# The Zerynth Device Manager is the entrypoint for the zCloud.
+# Let's connect and send data to the ZDM with a simple example.
+# Before exeuting this code, please associate the device with your
+# ZDM account by selecting "ZDM target" in VSCode Control Panel.
+
+# Let's import the zdm module
+from zdm import zdm
+# We also need cellular or ethernet
+from networking import cellular
+
+# Import serial module to talk with cellular component
+import serial
+
+# Initialize the board
+board.init()
+
+# Open and configure the cellular serial port
+_ = serial.serial(SERIAL1, baud=115200)
+modem = serial.serial(SERIAL2, baud=115200, flow_ctrl=serial.HW_FLOWCTRL_DISABLE)
+
+# Globals
+location = (0.0, 0.0, 0.0, 0.0, 0, 0.0, 0.0, 0.0, 0)
+
+while True:
+
+    try:
+        # Let's connect to the cellular
+        print("configuring cellular...")
+        cellular.configure(modem)
+        print("initializing cellular...")
+        cellular.init()
+
+        print("initializing cellular gps submodule...")
+        cellular.gps_start()
+        try:
+            # wait the initial fix, which may take some time
+            location = cellular.location(timeout=60)
+        except Exception as e:
+            print("Initial gps fix timed out: ", e)
+
+        print("connecting to cellular...")
+        cellular.start()
+        print("connected!",cellular.info())
+
+        # the Agent class implements all the logic to talk with the ZDM
+        agent = zdm.Agent()
+        # just start it
+        agent.start()
+
+        # Try to get GPS position with an adaptive fix timeout
+        c = 0
+        k = 5        # increment in seconds for adaptive timeout
+        max_incr = 4 # max increment steps for timeout
+        while True:
+            tout = 10+min(max_incr,c)*k
+            try:
+                location = cellular.location(timeout=tout)
+                lat = location[0]
+                lon = location[1]
+                c = 0
+            except Exception as e:
+                print("gps fix timed out: ", e, tout)
+                c += 1
+
+            # use the agent to publish values to the ZDM
+            # Just open the device page from VSCode and check that data is incoming
+            agent.publish({"latitude":lat, "longitude":lon})
+            sleep(5000)
+            print("latitude, longitude: ", lat, lon)
+            # The agent automatically handles connections and reconnections
+            print("ZDM is online:    ",agent.online())
+            # And provides info on the current firmware version
+            print("Firmware version: ",agent.firmware())
+
+        cellular.stop()
+        print("disconnected from cellular")
+    except CellularBadAPN:
+        print("Bad APN")
+        cellular.stop()
+        cellular.deinit()
+    except CellularModemInitError:
+        print("Modem initialization failed")
+        cellular.deinit()
+    except CellularException:
+        print("Generic Cellular Exception")
+        cellular.stop()
+        cellular.deinit()
+    except Exception as e:
+        print("Exception: ", e)
+        raise e
+
+    sleep(3000)
+
